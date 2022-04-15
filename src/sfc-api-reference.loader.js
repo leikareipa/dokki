5daf568a2e794b344e602ac52bc6ceedeac34295
leikareipa <drilledicecream@gmail.com>
2022-04-15 20:10:19 +0300

    sfc-api-reference-loader: Fix not working with webpack --watch

--- src/sfc-api-reference.loader.js
+++ src/sfc-api-reference.loader.js
@@ -5,7 +5,9 @@
  * 
  * 
  * A quick 'n dirty custom webpack loader for parsing dokki's <api-reference> documentation blocks
- * in Vue SFC files. The loader will insert the blocks' contents into a document template.
+ * in Vue SFC files. The loader will insert the blocks' contents into an API reference HTML template.
+ * 
+ * I haven't written a webpack loader before, so I'm going by the seat of my pants a bit here.
  * 
  * Usage in webpack.config.js:
  * 
@@ -19,48 +21,42 @@
  * 
  */
 
-const nodeHtmlParser = require("node-html-parser");
+const htmlParser = require("node-html-parser");
 const fs = require("fs");
 
-const processedResources = new Set();
-let htmlTemplate, htmlTemplateEl;
+const topics = {};
+let templateDoc, topicsContainerEl;
 
 module.exports = function (source) {
     const basePath = this.rootContext;
-    const resourceName = this.resourcePath;
-    const templateFilename = `${basePath}/src/docs/api-reference.template.html`;
-    const templateOutputFilename = `${basePath}/docs/api-reference.html`;
-    const doc = nodeHtmlParser.parse(source);
+    const srcFilename = this.resourcePath;
+    const dstFilename = `${basePath}/docs/api-reference.html`;
+    const sfcContents = htmlParser.parse(source);
 
-    if (!htmlTemplate) {
-        htmlTemplate = nodeHtmlParser.parse(
-            fs.readFileSync(templateFilename, "utf-8")
+    // Load the API reference HTML template as soon as we know the base path.
+    if (!templateDoc) {
+        templateDoc = htmlParser.parse(
+            fs.readFileSync(`${basePath}/src/docs/api-reference.template.html`, "utf-8")
         );
 
-        htmlTemplateEl = htmlTemplate.querySelector("dokki-topics");
+        topicsContainerEl = templateDoc.querySelector("dokki-topics");
     }
 
-    // I'm using a Set here to make sure the script doesn't process the same
-    // source files multiple times. I'm not sure why webpack does it, but it
-    // passes the same files into the loader three, four times. So this is a
-    // kludge for now.
-    if (!processedResources.has(resourceName)) {
-        processedResources.add(resourceName);
+    // Insert the custom block's contents into the output API reference file.
+    const docEl = sfcContents.querySelector("api-reference"); 
+    if (docEl) {
+        const componentName = srcFilename.match(/components\/(.*)\.vue$/)[1];
 
-        const docEl = doc.querySelector("api-reference");
+        topics[componentName] = topic_html_string(componentName, docEl.innerHTML);
+        topicsContainerEl.innerHTML = Object.keys(topics).sort().map(key=>topics[key]).join("\n");
 
-        if (docEl) {
-            const componentName = resourceName.match(/components\/(.*)\.vue$/)[1];
+        fs.writeFileSync(dstFilename, templateDoc.toString(), "utf-8");
 
-            htmlTemplateEl.innerHTML += topic_html_string(componentName, docEl.innerHTML);
-            fs.writeFileSync(templateOutputFilename, htmlTemplate.toString(), "utf-8");
-
-            // Don't pass the custom <api-reference> blocks to other loaders.
-            docEl.remove();
-        }
+        // Don't pass the custom block to other loaders.
+        docEl.remove();
     }
 
-    return doc.toString();
+    return sfcContents.toString();
 
     function topic_html_string(componentName = "", content = "")
     {
