f11805fa9502322b0c594aacadc2cd2315b8347d
leikareipa <drilledicecream@gmail.com>
2022-05-06 04:28:48 +0300

    Add script to build intermediate-format docs into distributable format
    The intermediate format is a new end-user format for authoring dokki
    documents, allowing the use of e.g. Markdown syntax, which is then built
    into a final, distributable HTML document.
    
    This is work in progress.

---
+++ src/tools/build-final.js
@@ -0,0 +1,446 @@
+/*
+ * 2022 Tarpeeksi Hyvae Soft
+ *
+ * Software: dokki
+ * 
+ * Converts dokki's intermediate-format documents into their final HTML format. Work
+ * in progress.
+ * 
+ * Command-line arguments:
+ * 
+ *   1: Path to the intermediate source HTML file.
+ *   2: Path to the output HTML file.
+ * 
+ *   Example: "node build-intermediate.js ./data/intermediate.html ./dist/output.html"
+ * 
+ */
+
+const fs = require("fs");
+const path = require("path");
+const assert = require("node:assert");
+const htmlParser = require("node-html-parser");
+const markdownIt = require("markdown-it")({html: true});
+
+// Run.
+(function()
+{
+    const srcHtmlFilename = process.argv[2];
+    const dstHtmlFilename = process.argv[3];
+    assert(srcHtmlFilename, "No source HTML file specified.");
+    assert(dstHtmlFilename, "No destination HTML file specified.");
+
+    const srcDOM = htmlParser.parse(fs.readFileSync(srcHtmlFilename, "utf-8"));
+    const dokkiEl = srcDOM.querySelector("dokki");
+    const topicsEl = srcDOM.querySelector("topics");
+    const titleEl = srcDOM.querySelector("title");
+    assert(dokkiEl, "The source HTML is missing a <dokki> block.");
+    assert(topicsEl, "The source HTML's <dokki> block is missing a <topics> child.");
+    assert(titleEl, "The source HTML's <dokki> block is missing a <title> child.");
+
+    const bodyEl = (()=>{
+        if (!srcDOM.querySelector("body"))
+        {
+            dokkiEl.insertAdjacentHTML("afterend", "<body></body>");
+        }
+        return srcDOM.querySelector("body");
+    })();
+    assert(bodyEl, "Could not find or create a <body> block in the source HTML.");
+
+    // Load the contents of <topics></topics> and convert them into DOM trees.
+    const topicsDOM = (()=>{
+        // The source HTML code is provided inline.
+        if (topicsEl.getAttribute("inline") !== undefined)
+        {
+            const lang = (topicsEl.getAttribute("lang") || "html");
+            const content = topicsEl.innerHTML;
+
+            switch (lang)
+            {
+                case "md": {
+                    const html = markdownIt.render(content);
+                    return [htmlParser.parse(html)];
+                }
+                case "html": {
+                    return [htmlParser.parse(content)];
+                }
+                default: assert.fail(`Unrecognized source öanguage ${lang}.`);
+            }
+        }
+        // The source HTML provides a list of files containing the code.
+        else
+        {
+            const topicsFilenameList = (()=>{
+                try {
+                    return eval(`[${topicsEl.innerHTML}]`);
+                }
+                catch (error) {
+                    return undefined;
+                }
+            })();
+
+            assert(
+                (Array.isArray(topicsFilenameList) && topicsFilenameList.length),
+                "The <topics> block contains an invalid source file array."
+            );
+            
+            return topicsFilenameList.map(filename=>{
+                const fileExtension = path.extname(filename);
+                const fileContents = fs.readFileSync(filename, "utf-8");
+
+                switch (fileExtension)
+                {
+                    case ".md": {
+                        const html = markdownIt.render(fileContents);
+                        return htmlParser.parse(html);
+                    }
+                    case ".html": {
+                        return htmlParser.parse(fileContents);
+                    }
+                    default: assert.fail(`Unrecognized source file type ${fileExtension}.`);
+                }
+            });
+        }
+    })();
+
+    // Modify the DOM trees loaded from <topics></topics> to use the dokki API (e.g.
+    // <dokki-table> instead of <table>).
+    const dokkiTopicsDOM = topicsDOM.map(topicDOM=>{
+        convert_h1_h2_to_dokki_topic_subtopic(topicDOM);
+        dokkify_code_blocks(topicDOM);
+        dokkify_tables(topicDOM);
+        dokkify_media(topicDOM);
+        move_dokki_elements_out_of_p(topicDOM);
+        integrate_standalone_captions(topicDOM);
+        dokkify_blockquotes(topicDOM);
+        merge_code_and_output(topicDOM);
+        return topicDOM;
+    });
+
+    // Inject dokki's element tree into the source HTML.
+    {
+        const docTitle = titleEl.textContent;
+        const topicsHtml = dokkiTopicsDOM.reduce((html, dom)=>(html + dom.outerHTML), "");
+
+        bodyEl.insertAdjacentHTML("beforeend", `
+            <template id="dokki-document">
+                <dokki-toolbar>
+                    <template #caption>
+                        ${docTitle}
+                    </template>
+                </dokki-toolbar>
+                <dokki-topics>
+                    ${topicsHtml}
+                </dokki-topics>
+            </template>
+        `);
+
+        dokkiEl.remove();
+    }
+
+    fs.writeFileSync(dstHtmlFilename, srcDOM.outerHTML);
+
+    process.exit(0);
+})();
+
+// Merges adjacent <dokki-code> and <dokki-xxxx inline> elements, whereby the
+// latter element becomes the <dokki-code> element's output.
+function merge_code_and_output(dom)
+{
+    const embeddableWithCode = [
+        "dokki-image",
+        "dokki-iframe",
+        "dokki-directory",
+        "dokki-spoiler",
+        "dokki-output",
+        "dokki-table",
+        "dokki-video"
+    ];
+
+    const els = dom.querySelectorAll(embeddableWithCode.map(tag=>`dokki-code + ${tag}[inline]`).join(","));
+
+    for (const el of els)
+    {
+        const codeEl = el.previousElementSibling;
+        codeEl.insertAdjacentHTML("beforeend", `<template #output>${el.outerHTML}</template>`);
+        el.remove();
+    }
+}
+
+// Converts
+//
+//   <p>Abcd <dokki-xxxx>...</dokki-xxxx></p>
+//
+// into
+//
+//   <p>Abcd</p>
+//   <dokki-xxxx>...</dokki-xxxx>
+//
+// (<dokki-xxxx> elements are converted into <p> elements by dokki at run-time,
+// so it doesn't make sense to have them wrapped in a duplicate <p> by Markdown).
+function move_dokki_elements_out_of_p(dom)
+{
+    const dokkiElTags = [
+        "dokki-image",
+        "dokki-code",
+        "dokki-iframe",
+        "dokki-directory",
+        "dokki-spoiler",
+        "dokki-output",
+        "dokki-table",
+        "dokki-video"
+    ];
+
+    const els = dom.querySelectorAll(dokkiElTags.map(tag=>`p > ${tag}`).join(","));
+
+    for (const el of els)
+    {
+        el.parentNode.insertAdjacentHTML("afterend", el);
+        el.parentNode.remove();
+    }
+}
+
+// Converts
+//
+//   <table>
+//     Abcd
+//   </table>
+//
+// into
+//
+//   <dokki-table>
+//     <template #table>
+//       Abcd
+//     </template>
+//   </dokki-table>
+function dokkify_tables(dom)
+{
+    const tableEls = dom.querySelectorAll("table");
+    
+    for (const el of tableEls)
+    {
+        el.insertAdjacentHTML("beforebegin", `<dokki-table><template #table>${el.innerHTML}</template></dokki-table>`);
+        el.remove();
+    }
+}
+
+// Converts
+//
+//   <img src="Abcd" alt="<100x200> Efgh">
+//
+// into
+//
+//   <dokki-image src="Abcd" width="100" height="200">
+//     <template #caption>
+//       Efgh
+//     </template>
+//   </dokki-image>
+function dokkify_media(dom)
+{
+    const mediaEls = dom.querySelectorAll("img");
+    
+    for (const el of mediaEls)
+    {
+        const src = el.getAttribute("src");
+        let caption = el.getAttribute("alt");
+        const isInline = (()=>{
+            if (typeof caption !== "string") {
+                return false;
+            }
+            const exp = /{inline}/;
+            if (caption.match(exp)) {
+                caption = caption.replace(exp, "");
+                return true;
+            }
+            return false;
+        })();
+        const isYouTube = (()=>{
+            if (typeof caption !== "string") {
+                return false;
+            }
+            const exp = /{video:youtube}/;
+            if (caption.match(exp)) {
+                caption = caption.replace(exp, "");
+                return true;
+            }
+            return false;
+        })();
+        const [width, height] = (()=>{
+            if (typeof caption !== "string") {
+                return [];
+            }
+            const exp = /{image:(.*?x.*?)} ?/;
+            const resolution = caption.match(exp)?.[1]?.split("x");
+            if (resolution?.length === 2) {
+                caption = caption.replace(exp, "");
+                return resolution;
+            }
+            return [];
+        })();
+
+        caption = caption.trim().length
+            ? `<template #caption>${caption.trim()}</template>`
+            : "";
+
+        // The 'inline' attribute just needs to be present if we want the element to be inline.
+        // Otherwise, it needs to be undefined.
+        const inline = isInline
+            ? "inline=''"
+            : "inline=undefined";
+        
+        const dokkiMediaElString = (()=>{
+            if (isYouTube)
+            {
+                return `
+                    <dokki-video platform="youtube" src=${src} ${inline}>
+                        ${caption}
+                    </dokki-video>
+                `;
+            }
+            else
+            {
+                return `
+                    <dokki-image src=${src} width=${width} height=${height} ${inline}>
+                        ${caption}
+                    </dokki-image>
+                `;
+            }
+        })();
+
+        el.insertAdjacentHTML("beforebegin", dokkiMediaElString);
+        el.remove();
+    }
+}
+
+// Converts
+//
+//   <blockquote>
+//     Abcd
+//   <blockquote>
+//
+// into
+//
+//   <dokki-area>
+//     Abcd
+//   </dokki-area>
+function dokkify_blockquotes(dom)
+{
+    // We loop over the quoteSelectorAll() result to ensure we convert all nested
+    // blockquotes as well.
+    let quoteEls;
+    while ((quoteEls = dom.querySelectorAll("blockquote")).length)
+    {
+        for (const el of quoteEls)
+        {
+            el.insertAdjacentHTML("beforebegin", `<dokki-area>${el.innerHTML}</dokki-area>`);
+            el.remove();
+        }
+    }
+}
+
+// Converts
+//
+//   <pre>
+//     <code>
+//       Abcd
+//     </code>
+//   </pre>
+//
+// into
+//
+//   <dokki-code code="Abcd">
+//   </dokki-code>
+function dokkify_code_blocks(dom)
+{
+    const codeEls = dom.querySelectorAll("pre").filter(el=>el.textContent);
+
+    for (const el of codeEls)
+    {
+        const code = htmlParser
+            .parse(el.textContent)
+            .querySelector("code");
+
+        if (code)
+        {
+            const codeSyntax = code.classList.value[0]?.substring("language-".length);
+            const codeText = code.innerHTML.replaceAll("\"", "``");
+            el.insertAdjacentHTML("beforebegin", `<dokki-code syntax="${codeSyntax}" code="${codeText}"></dokki-code>`);
+            el.remove();
+        }
+    }
+}
+
+// Converts
+//
+//   <p><em>Abcd</em></p>
+//   <dokki-xxxx></dokki-xxxx>
+//
+// into
+//
+//   <dokki-xxxx>
+//     <template #caption>Abcd</template>
+//   </dokki-xxxx>"
+function integrate_standalone_captions(dom)
+{
+    const captionableEls = dom.querySelectorAll(`
+        dokki-image,
+        dokki-code,
+        dokki-iframe,
+        dokki-directory,
+        dokki-spoiler,
+        dokki-output,
+        dokki-table,
+        dokki-video
+    `).filter(el=>el.previousElementSibling?.rawTagName === "blockquote");
+
+    for (const el of captionableEls)
+    {
+        const captionEl = el.previousElementSibling;
+
+        if (captionEl) {
+            const caption = captionEl.textContent.trim();
+            el.insertAdjacentHTML("afterbegin", `<template #caption>${caption}</template>`);
+            captionEl.remove();
+        }
+    }
+}
+
+function convert_h1_h2_to_dokki_topic_subtopic(dom)
+{
+    const html = dom.innerHTML.split("\n");
+
+    let isInsideTopic = false;
+    let isInsideSubtopic = false;
+
+    for (let i = 0; i < html.length; i++)
+    {
+        if (html[i].startsWith("<h1>"))
+        {
+            if (isInsideSubtopic)
+            {
+                html[i] = ("</dokki-subtopic>" + html[i]);
+                isInsideSubtopic = false;
+            }
+
+            // Note: We rely on the HTML parser to clean up unneeded closing tags.
+            html[i] = html[i].replace(/<h1>(.*)<\/h1>/, "</dokki-topic><dokki-topic title='$1'>");
+            isInsideTopic = true;
+        }
+        else if (html[i].startsWith("<h2>"))
+        {
+            html[i] = html[i].replace(/<h2>(.*)<\/h2>/, "</dokki-subtopic><dokki-subtopic title='$1'>");
+            isInsideSubtopic = true;
+        }
+    }
+
+    if (isInsideSubtopic)
+    {
+        html[html.length-1] += "</dokki-subtopic>";
+    }
+
+    if (isInsideTopic)
+    {
+        html[html.length-1] += "</dokki-topic>";
+    }
+
+    dom.innerHTML = html.join("\n");
+}
