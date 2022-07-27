0a1337dbb629b5dc4a0d43262d7287f44c437773
leikareipa <drilledicecream@gmail.com>
2022-07-27 20:27:57 +0300

    dokki0-topic: Disable left border on <dokki-tip/-warning> when...
    ...not a direct child of the topic parent.

--- src/components/dokki0-topic.vue
+++ src/components/dokki0-topic.vue
@@ -132,6 +132,15 @@
         font-style: italic;
     }
 
+    /* Disable the left border on <dokki-tip> and <dokki-warning> when they're not direct
+       children of their parent topic. This is to prevent some visual inconsistencies that
+       could otherwise occur.*/
+    & > * .dokki-tip,
+    & > * .dokki-warning
+    {
+        box-shadow: none !important;
+    }
+
     body[data-dokki-layout="horizontal-narrow"] &,
     body[data-dokki-layout^="vertical"] &
     {
